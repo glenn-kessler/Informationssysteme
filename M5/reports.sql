1f9b15a88fd59476146dd3b919838ca8e74bf1df
Marvin Wiegand <mflipw@users.noreply.github.com>
2017-01-17 15:16:17 +0100

    implements report #22 PROVIDED SERVICES

--- M5/reports.sql
+++ M5/reports.sql
@@ -79,7 +79,28 @@ ON LB.DozentID = PROF.DozentID
 /*******************************************
 ** 22. provided services
 *******************************************/
-
+select fak.teilgebiet, tmp.lastname, fak.sws, modul_fak
+from ( select distinct teilgebiet
+				--, m.spoid
+				, fakultaetid as modul_fak
+				, sws 
+			from glkeit00_modul_spo as m
+	join glkeit00_spo as s
+	on s.spoid = m.spoid
+) as fak
+join ( select distinct 
+		teilgebiet
+		, h.dozentid
+		, h.lastname
+		, fakultaetid as dozent_fak
+	from (select distinct dozentid, lastname, teilgebiet from glkeit00_hat) as h
+	join glkeit00_dozent as d
+	on h.dozentid = d.dozentid
+	and h.lastname = d.lastname
+) as tmp
+on fak.teilgebiet = tmp.teilgebiet
+where fak.modul_fak not like 'IT'
+and tmp.dozent_fak = 'IT'
 
 /*******************************************
 ** 23. used services
